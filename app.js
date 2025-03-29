0c9f9ea1554db54679d0029c76841b387a2da926
barkamol-valiy <barkamol.valiy@gmail.com>
2025-03-29 08:53:44 +0500

    project setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+  Menu.setApplicationMenu(null);
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
